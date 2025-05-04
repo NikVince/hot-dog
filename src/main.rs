34fbfc0825c14ae167c581aeae3a1ee75fb9729f
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-05-04 23:30:24 +0200

    UPDATED main.rs with basic component implementation.

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,18 @@ use dioxus::prelude::*;
 fn main() {
     dioxus::launch(App);
 }
+
+#[component]
+fn App() -> Element {
+    rsx! { "HotDog!" }
+}
+
+#[component]
+fn DogApp(props: DogAppProps) -> Element {
+    todo!()
+}
+
+#[derive(Props, PartialEq, Clone)]
+struct DogAppProps {
+    breed: String,
+}
