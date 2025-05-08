64d951de2b4aaf9ca72d2f6ddc4b52feca247f86
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-08 22:11:24 +0200

    UPDATED main.rs to add basic logic for fetching mew images from the 'dog.ceo/dog-api'.

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,29 @@ fn Title() -> Element {
     }
 }
 
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
 #[component]
 fn DogView() -> Element {
+    let mut img_src = use_signal(|| "".to_string());
+
+    let fetch_new = move |_| async move {
+        let response = reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap();
+
+        img_src.set(response.message);
+    };
+
     let skip = move |evt| {};
     let save = move |evt| {};
+
     let img_src = use_hook(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
 
     rsx! {
@@ -36,7 +55,7 @@ fn DogView() -> Element {
         }
         div { id: "buttons",
             button { onclick: skip, id: "skip", "skip" }
-            button { onclick: save, id: "save", "save!" }
+            button { onclick: fetch_new, id: "save", "save!" }
         }
     }
 }
