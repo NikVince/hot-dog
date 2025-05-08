55bea960d90bd123477c4e37d7b46e2b56dd358e
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-08 22:46:39 +0200

    CREATED guide_backend to handle basic saving to 'dogs.txt' functionality as backend.

---
+++ src/guide_backend.rs
@@ -0,0 +1,18 @@
+// Expose a `save_dog` endpoint on our server that takes an "image" parameter
+#[server]
+async fn save_dog(image: String) -> Result<(), ServerFnError> {
+    use std::io::Write;
+
+    // Open the `dogs.txt` file in append-only mode, creating it if it doesn't exist;
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("dogs.txt")
+        .unwrap();
+
+    // And then write a newline to it with the image url
+    file.write_fmt(format_args!("{image}\n"));
+
+    Ok(())
+}
