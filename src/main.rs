50362bba95935ca29256dc935b45c9645f414ee8
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-08 22:28:54 +0200

    UPDATED main.rs to add functionality of images updating upon each button press.

--- src/main.rs
+++ src/main.rs
@@ -31,31 +31,23 @@ struct DogApi {
 
 #[component]
 fn DogView() -> Element {
-    let mut img_src = use_signal(|| "".to_string());
-
-    let fetch_new = move |_| async move {
-        let response = reqwest::get("https://dog.ceo/api/breeds/image/random")
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
             .await
             .unwrap()
             .json::<DogApi>()
             .await
-            .unwrap();
-
-        img_src.set(response.message);
-    };
-
-    let skip = move |evt| {};
-    let save = move |evt| {};
-
-    let img_src = use_hook(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
+            .unwrap()
+            .message
+    });
 
     rsx! {
         div { id: "dogview",
-            img { src: "{img_src}" }
+            img { src: img_src.cloned().unwrap_or_default() }
         }
         div { id: "buttons",
-            button { onclick: skip, id: "skip", "skip" }
-            button { onclick: fetch_new, id: "save", "save!" }
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
         }
     }
 }
