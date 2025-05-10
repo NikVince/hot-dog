6fedbda429f01d270eed46a522f95831a0fd90ea
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-10 14:43:22 +0200

    UPDATED various files to refactor and eliminate compilation preventing errors.

--- src/guide_databases.rs
+++ src/guide_databases.rs
@@ -25,3 +25,18 @@ pub async fn save_dog(image: String) -> Result<(), ServerFnError> {
     DB.with(|f| f.execute("INSERT INTO dogs (url) VALUES (?1)", &[&image]))?;
     Ok(())
 }
+
+// Query the database and return the last 10 dogs and their url
+#[server(ListDogs)]
+pub async fn list_dogs() -> Result<Vec<(usize, String)>, ServerFnError> {
+    let dogs = DB.with(|f| {
+        f.prepare("SELECT id, url FROM dogs ORDER BY id DESC LIMIT 10")
+            .unwrap()
+            .query_map([], |row| Ok((row.get(0)?, row.get(1)?)))
+            .unwrap()
+            .map(|r| r.unwrap())
+            .collect()
+    });
+
+    Ok(dogs)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use crate::Route;
 use dioxus::prelude::*;
 
 // Server side imports
@@ -97,7 +96,7 @@ pub fn NavBar() -> Element {
 pub fn Favorites() -> Element {
     // Create a pending resource that resolves to the list of dogs from the backend
     // Wait for the favorites list to resolve with `.suspend()`
-    let mut favorites = use_resource(super::backend::list_dogs).suspend()?;
+    let mut favorites = use_resource(guide_databases::list_dogs).suspend()?;
 
     rsx! {
         div { id: "favorites",
